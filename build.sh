2d63ed8f76e925aa3358b9a5094569d39d697279
fedemoreno22 <101230895+fedemoreno22@users.noreply.github.com>
2024-03-08 17:30:41 -0300

    req ver

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ set -o errexit
 
 # poetry install
 pip install -r requirements.txt
-py -m pip install pywin32==306
+pip install pywin32==306
 pip install --upgrade pip
 
 python manage.py collectstatic --no-input
