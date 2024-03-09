e3fb36a7d6dcd838f35e5e517534591e034dac44
fedemoreno22 <101230895+fedemoreno22@users.noreply.github.com>
2024-03-08 23:36:56 -0300

    new version

--- build.sh
+++ build.sh
@@ -4,8 +4,6 @@ set -o errexit
 
 # poetry install
 pip install -r requirements.txt
-pip install pywin32==306
-pip install --upgrade pip
 
 python manage.py collectstatic --no-input
 python manage.py migrate
